3d1b4a66bb6d219747772c85d5b34b11a63aeb10
Tayfun Sur <tayfunsur@gmail.com>
2020-01-16 18:05:46 +0300

    basic calculations and filling the display screen are done.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,36 @@
+const screenAfterButtonClick = document.querySelector('p');
+screenAfterButtonClick.className = "result";
+const buttons = [...document.querySelectorAll('button')];
+
+buttons.forEach(button => {
+    button.addEventListener('click', display);
+});
+
+let finalDecision = "";
+let firstParam = "";
+let secondParam = "";
+function display(){
+    if ((this.textContent == "*") || (this.textContent == "/") || (this.textContent == "-") ||
+    (this.textContent == "+")) {
+        
+        firstParam = parseInt(screenAfterButtonClick.textContent);
+        screenAfterButtonClick.textContent = "";
+        finalDecision = this.textContent;
+        
+    } else if (this.textContent == "=") {
+        secondParam = parseInt(screenAfterButtonClick.textContent);
+        screenAfterButtonClick.textContent = operate(firstParam,secondParam,finalDecision);     
+    } else if (this.textContent == "C"){
+        finalDecision = "";
+        firstParam = "";
+        secondParam ="";
+        screenAfterButtonClick.textContent = "";
+    }else {
+        screenAfterButtonClick.textContent += this.textContent;
+    }
+}
+//|| (this.textContent == "C")) bunu sonra koy
+
 function add(a,b) {
     return a+b;
 }
@@ -30,3 +63,4 @@ function operate(a,b,c) {
             break;        
     }
 }
+
