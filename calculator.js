471fccacc3f8a439713e4a8463f4cfa6cdaf0233
Tayfun Sur <tayfunsur@gmail.com>
2020-01-17 14:36:25 +0300

    Function added

--- calculator.js
+++ calculator.js
@@ -6,61 +6,22 @@ buttons.forEach(button => {
     button.addEventListener('click', display);
 });
 
-let finalDecision = "";
-let firstParam = "";
-let secondParam = "";
+
+function displayResult(resultScreen){
+    return new Function('"use strict";return ' + resultScreen)();      
+}
+
+
 function display(){
-    if ((this.textContent == "*") || (this.textContent == "/") || (this.textContent == "-") ||
-    (this.textContent == "+")) {
-        
-        firstParam = parseInt(screenAfterButtonClick.textContent);
-        screenAfterButtonClick.textContent = "";
-        finalDecision = this.textContent;
-        
-    } else if (this.textContent == "=") {
-        secondParam = parseInt(screenAfterButtonClick.textContent);
-        screenAfterButtonClick.textContent = operate(firstParam,secondParam,finalDecision);     
-    } else if (this.textContent == "C"){
+    if (this.textContent == "C"){
         finalDecision = "";
         firstParam = "";
         secondParam ="";
         screenAfterButtonClick.textContent = "";
-    }else {
-        screenAfterButtonClick.textContent += this.textContent;
+    } else if (this.textContent != "="){
+    screenAfterButtonClick.textContent += this.textContent;  
+    } else if (this.textContent == "=") {
+        displayResult(screenAfterButtonClick.textContent);
+        screenAfterButtonClick.textContent = displayResult(`${screenAfterButtonClick.textContent}`);
     }
 }
-//|| (this.textContent == "C")) bunu sonra koy
-
-function add(a,b) {
-    return a+b;
-}
-
-function substract(a,b) {
-    return a-b;
-}
-
-function multiply(a,b) {
-    return a*b;   
-}
-
-function divide(a,b) {
-    return a/b;
-}
-
-function operate(a,b,c) {
-    switch (c) {
-        case "+":
-            return add(a,b);
-            break;
-        case "-":
-            return substract(a,b);
-            break;
-        case "*":
-            return multiply(a,b);
-            break;
-        case "/":
-            return divide(a,b);
-            break;        
-    }
-}
-
