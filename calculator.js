a38075681b5f6604065415d33c1b5b5515803412
Tayfun Sur <tayfunsur@gmail.com>
2020-01-17 16:03:37 +0300

    fixed the dot button

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 const screenAfterButtonClick = document.querySelector('p');
 screenAfterButtonClick.className = "result";
 const buttons = [...document.querySelectorAll('button')];
+const dotbutton = document.querySelector('button[id="."]');
+
 
 buttons.forEach(button => {
     button.addEventListener('click', display);
@@ -14,12 +16,25 @@ function displayResult(resultScreen){
 
 function display(){
     if (this.textContent == "C"){
-        finalDecision = "";
-        firstParam = "";
-        secondParam ="";
         screenAfterButtonClick.textContent = "";
+    } else if((this.textContent == "*") || (this.textContent == "/") || (this.textContent == "+") || (this.textContent == "-")) {
+        if ((screenAfterButtonClick.textContent.substr(-1) == "*") || (screenAfterButtonClick.textContent.substr(-1) == "/")
+        || (screenAfterButtonClick.textContent.substr(-1) == "+") || (screenAfterButtonClick.textContent.substr(-1) == "-")) {
+            screenAfterButtonClick.textContent = screenAfterButtonClick.textContent.replace(/.$/,this.textContent);
+        } else if (screenAfterButtonClick.textContent.substr(-1) != "."){
+            screenAfterButtonClick.textContent += this.textContent;
+            dotbutton.disabled = false; 
+        } else {
+            screenAfterButtonClick.textContent += this.textContent;
+        }    
+    } else if(this.textContent == "."){
+        console.log(screenAfterButtonClick.textContent.substr(-1));
+        if (screenAfterButtonClick.textContent.substr(-1) != "."){
+            screenAfterButtonClick.textContent += this.textContent;
+            dotbutton.disabled = true;
+        }
     } else if (this.textContent != "="){
-    screenAfterButtonClick.textContent += this.textContent;  
+    screenAfterButtonClick.textContent += this.textContent;
     } else if (this.textContent == "=") {
         displayResult(screenAfterButtonClick.textContent);
         screenAfterButtonClick.textContent = displayResult(`${screenAfterButtonClick.textContent}`);
