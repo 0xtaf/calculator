f21c25454e1203383e40b56c7fdc42b69f742161
Tayfun Sur <tayfunsur@gmail.com>
2020-01-18 12:45:53 +0300

    control variable for cleaning after calculation is added

--- calculator.js
+++ calculator.js
@@ -8,16 +8,21 @@ buttons.forEach(button => {
     button.addEventListener('click', display);
 });
 
-
+let control = 0;
 function displayResult(resultScreen){
-    return new Function('"use strict";return ' + resultScreen)();      
+    let finalResult = new Function('"use strict";return ' + resultScreen)();    
+    // return finalResult.toPrecision(2);  
+    return finalResult;
 }
 
+//sonuçtan sonra input girilirse baştan yazsın
 
 function display(){
+    
     if (this.textContent == "C"){
         screenAfterButtonClick.textContent = "";
     } else if((this.textContent == "*") || (this.textContent == "/") || (this.textContent == "+") || (this.textContent == "-")) {
+        control = 0;
         if ((screenAfterButtonClick.textContent.substr(-1) == "*") || (screenAfterButtonClick.textContent.substr(-1) == "/")
         || (screenAfterButtonClick.textContent.substr(-1) == "+") || (screenAfterButtonClick.textContent.substr(-1) == "-")) {
             screenAfterButtonClick.textContent = screenAfterButtonClick.textContent.replace(/.$/,this.textContent);
@@ -26,7 +31,8 @@ function display(){
             dotbutton.disabled = false; 
         } else {
             screenAfterButtonClick.textContent += this.textContent;
-        }    
+        }
+          
     } else if(this.textContent == "."){
         console.log(screenAfterButtonClick.textContent.substr(-1));
         if (screenAfterButtonClick.textContent.substr(-1) != "."){
@@ -34,9 +40,16 @@ function display(){
             dotbutton.disabled = true;
         }
     } else if (this.textContent != "="){
+        if (control == 1){
+            if ((this.textContent != "*") || (this.textContent != "/") || (this.textContent != "+") || (this.textContent != "-")){
+                screenAfterButtonClick.textContent = "";
+                control = 0;
+            }
+        } 
     screenAfterButtonClick.textContent += this.textContent;
     } else if (this.textContent == "=") {
         displayResult(screenAfterButtonClick.textContent);
         screenAfterButtonClick.textContent = displayResult(`${screenAfterButtonClick.textContent}`);
+        control = 1;
     }
 }
